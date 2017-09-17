6cf14546196439c3a8e88bba23ca9aa41ef63590
Nadya Febiana Djojosantoso <hello@nadyafebi.com>
2017-09-17 13:20:21 -0700

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var panini = require('panini');
 
-gulp.task('default', function() {
-  gulp.src('src/pages/**/*.html')
+gulp.task('default',
+  gulp.series(build, watch));
+
+// Use Panini to convert pages to finished pages.
+function build() {
+  return gulp.src('src/pages/**/*.html')
     .pipe(panini({
       root: 'src/pages/',
       layouts: 'src/layouts/',
       partials: 'src/partials/',
-      helpers: 'src/helpers/',
-      data: 'src/data/'
+      helpers: 'src/helpers',
+      data: 'src/data'
     }))
     .pipe(gulp.dest('.'));
-});
+}
 
-gulp.watch('src/{pages,layouts,partials,helpers,data}/**/*.html', ['default']);
+// Load updated templates to Panini.
+function reset(done) {
+  panini.refresh();
+  done();
+}
+
+function watch() {
+  gulp.watch('src/pages/**/*.html').on('all', gulp.series(build));
+  gulp.watch('src/{layouts,partials,helpers,data}/**/*').on('all', gulp.series(reset, build));
+}
